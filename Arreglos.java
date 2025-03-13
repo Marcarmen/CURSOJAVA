e11d5dd6bb2c4d8ed4bf06bffd3bcc9daf45de84
Marcarmen <164301606+Marcarmen@users.noreply.github.com>
2025-03-13 12:13:01 -0600

    Add files via upload

---
+++ Arreglos.java
@@ -0,0 +1,26 @@
+/*TEMA: ARREGLOS
+ * NOMBRE: MARIA DEL CARMEN LIMA GARCIA
+ * CAPACITADOR: VICTOR SAUCEDO CASTILLO
+ *
+ * DEFINICIÓN: Un arreglo permite almacenar varios valores del mismo tipo en una sola variable.
+También se le conoce como array.
+
+ */
+
+public class Arreglos {
+    /*MÉTODO PRINCIPAL*/
+    public static void main(String[] args) {
+        /*PASO 1. DECLARAMOS UN ARREGLO, DE TIPO ENTERO, EL CUAL LLEVA POR
+         NOMBRE ENTERO*/
+
+        int[] entero;
+
+        /*INICIALIZAMOS EL ARREGLO
+        * NUESTRO ARREGLO RESERVA 3 ELEMENTOS*/
+        entero = new int[3];
+
+        /*2DA FORMA DECLARACIÓN DE UN ARREGLO*/
+         var numeros = new int [2];
+
+    }
+}
